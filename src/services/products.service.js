60924d65e24e8428275d5cfd861481a16c71329d
Kirill Vasilkov <vasilkov.kv@perfema.com>
2021-11-14 23:58:54 +0300

    fix(products-service): add title property

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -5,6 +5,7 @@ export class ProductsApiService {
   #normalize(products) {
     return products.map((product) => ({
       id: product.id,
+      title: product.dish,
       image: ImageService.getRandomImage(),
       description: product.description,
       price: Math.floor(Math.random() * (1000 - 10) + 10),
