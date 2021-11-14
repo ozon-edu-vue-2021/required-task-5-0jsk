0eac96a0134c88c64a2d3435aa2367ed8dd88649
0jsk <90405410+0jsk@users.noreply.github.com>
2021-11-15 02:06:45 +0300

    feat(cart): layout

--- src/modules/cart/components/Cart.vue
+++ src/modules/cart/components/Cart.vue
@@ -1,11 +1,23 @@
 <template>
-  <ul class="cart">
-    <li class="cart__item" v-for="product in cart" :key="product.id">
-      <span>
-        {{ product.name }}
-      </span>
-    </li>
-  </ul>
+  <div class="cart">
+    <ul class="cart__item-list">
+      <li class="cart__item item" v-for="product in cart" :key="product.id">
+        <span class="item__title">
+          {{ product.title }}
+        </span>
+        <span>
+          {{ product.count }}
+        </span>
+      </li>
+    </ul>
+    <button
+      type="button"
+      v-if="cart.length > 0"
+      class="cart__btn-order btn-order"
+    >
+      Перейти в корзину
+    </button>
+  </div>
 </template>
 
 <script>
@@ -20,5 +32,44 @@ export default {
 </script>
 
 <style scoped>
+.cart {
+  display: flex;
+  flex-direction: column;
+}
 
+.cart__item-list {
+  width: 100%;
+}
+
+.cart__item {
+  margin-top: 6px;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+}
+
+.item__title {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  width: 75%;
+}
+
+.cart__btn-order {
+  margin-top: 8px;
+}
+
+.btn-order {
+  padding: 8px;
+
+  background-color: forestgreen;
+  border-radius: 4px;
+  border: unset;
+  color: #fff;
+  cursor: pointer;
+}
 </style>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -3,17 +3,20 @@
     <Products />
     <div class="panel__controls controls">
       <h3 class="controls__title">Корзина</h3>
+      <Cart class="panel__cart" />
     </div>
   </div>
 </template>
 
 <script>
 import { Products } from "@/modules/products/components";
+import { Cart } from "@/modules/cart/components";
 
 export default {
   name: "Catalog",
   components: {
     Products,
+    Cart,
   },
 };
 </script>
@@ -37,5 +40,10 @@ export default {
 
 .controls__title {
   margin: 0 auto;
+}
+
+.panel__cart {
+  width: 250px;
+  margin-left: 16px;
 }
 </style>
